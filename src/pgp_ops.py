b3c692f4885ab4e996444ad9b31cfde3b16b2a97
Alex Lee <cloudstrife79@gmail.com>
2023-03-03 15:50:56 -0500

    add functionality for adding user info to PGP keypair w/ hardcoded options

--- src/pgp_ops.py
+++ src/pgp_ops.py
@@ -11,3 +11,16 @@ def gen_keypair():
 
 # TODO: functionality to add user info to key, basic operations for signing / verifying, encrypting / decrypting, etc.
 # limit this for now to what is useful for the pterm project; not all pgpy functionality is needed
+
+def add_user_info(keypair, username, email=None):
+    """
+    Add user information to a key pair.
+    Hardcodes some options here for now; we can make these configurable later.
+    """
+    uid = pgpy.PGPUID.new(username, comment='pterm PGP key', email=email)
+    keypair.add_uid(uid,
+                    usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
+                    hashes=[HashAlgorithm.SHA256],
+                    ciphers=[SymmetricKeyAlgorithm.AES256],
+                    compression=[CompressionAlgorithm.ZLIB, CompressionAlgorithm.BZ2, CompressionAlgorithm.ZIP, CompressionAlgorithm.Uncompressed])
+    return keypair
